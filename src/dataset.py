fdef09774373cebfc1fdf67f44e17b5afd9877c4
Yifeng He <heeafen0602@gmail.com>
2024-05-07 16:37:00 -0700

    init files

---
+++ src/dataset.py
@@ -0,0 +1,116 @@
+"""main script to build Haskell dataset"""
+
+import fire
+from hs_parser import HASKELL_LANGUAGE
+from hs_parser.ast_util import AST, HaskellFunction
+from hs_parser.polymorphism import get_polymorphic_type, PolymorphicType
+from returns.result import Success, Failure
+from returns.io import IOResult, IOSuccess, IOFailure
+from funcy_chain import Chain
+import os
+import logging
+from common import wrap_repo
+from enum import IntEnum
+import json
+from pathos.multiprocessing import ProcessPool
+from tqdm import tqdm
+
+
+class CollectionErrorCode(IntEnum):
+    REPO_NOT_FOUND = 0
+    FUNC_NOT_FOUND = 1
+    SKIPPED = 2
+
+
+def collect_hs_files(root: str):
+    """Get all files end with .hs in the given root directory
+
+    Args:
+        root (str): path to repo root
+    """
+    for dirpath, _, filenames in os.walk(root):
+        for filename in filenames:
+            if filename.endswith(".hs") and os.path.isfile(
+                p := os.path.join(dirpath, filename)
+            ):
+                yield p
+
+
+def collect_from_file(file_path: str) -> list[dict[str, str]]:
+    with open(file_path, "r", errors="replace") as fp:
+        code = fp.read()
+
+    ast = AST(code, HASKELL_LANGUAGE)
+
+    def _to_json(func: HaskellFunction) -> dict[str, str]:
+        func_id = f"{file_path}--{ast.get_fn_name(func.type_signature).value_or(None)}"
+        return {
+            "id": func_id,
+            "code": ast.func2src(func),
+            "type": get_polymorphic_type(func.type_signature),
+        }
+
+    fs: list[dict[str, str]] = Chain(ast.get_functions()).map(_to_json).value
+    return fs
+
+
+def collect_from_repo(
+    repo_id: str, repo_root: str, source_root: str
+) -> IOResult[int, CollectionErrorCode]:
+    repo_path = os.path.join(repo_root, wrap_repo(repo_id))
+    if not os.path.exists(repo_path) or not os.path.isdir(repo_path):
+        return IOFailure(CollectionErrorCode.REPO_NOT_FOUND)
+
+    source_path = os.path.join(source_root, wrap_repo(repo_id) + ".jsonl")
+
+    # skip if already exist
+    if os.path.exists(source_path):
+        return IOFailure(CollectionErrorCode.SKIPPED)
+
+    # collect potential functions
+    all_functions = (
+        Chain(collect_hs_files(repo_path))
+        .mapcat(collect_from_file)
+        .map(json.dumps)
+        .value
+    )
+
+    if not all_functions:
+        return IOFailure(CollectionErrorCode.FUNC_NOT_FOUND)
+
+    # save to disk
+    with open(source_path, "w") as fp:
+        fp.write("\n".join(all_functions) + "\n")
+    return IOSuccess(len(all_functions))
+
+
+def main(
+    input_repo_list_path: str = "data/meta/haskell.txt",
+    repo_root: str = "data/repos",
+    oroot: str = "data/source/",
+):
+    with open(input_repo_list_path) as fp:
+        repo_id_list = [l.strip() for l in fp.readlines()]
+
+    logging.info(f"Loaded {len(repo_id_list)} repos to be processed")
+    num_func = 0
+    failed = [0, 0, 0]
+    for repo in tqdm(repo_id_list):
+        match collect_from_repo(repo, repo_root, oroot):
+            case IOSuccess(Success(n)):
+                num_func += n
+            case IOFailure(Failure(status)):
+                failed[status] += 1
+
+    if sum(failed):
+        failed_types = ["repo not found", "function not found", "skipped"]
+        failed_dict = {key: val for key, val in zip(failed_types, failed) if val != 0}
+        logging.warning(f"Failed: {failed_dict}")
+    logging.info(
+        f"Collected {num_func} functions from {len(repo_id_list)} repositories."
+    )
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    fire.Fire(main)
---
+++ src/hs_parser/ast_util.py
@@ -0,0 +1,149 @@
+"""Helper Class and Functions for tree-sitter AST"""
+
+from tree_sitter import Language, Parser, Tree
+from tree_sitter.binding import Node
+from returns.maybe import Maybe, Nothing, Some
+from dataclasses import dataclass
+from funcy_chain import Chain
+
+
+@dataclass
+class ASTLoc:
+    lineno: int  # line number in the source file
+    col: int  # column offset in the source file
+
+
+@dataclass
+class HaskellFunction:
+    type_signature: Node
+    functions: list[Node]
+
+    @staticmethod
+    def from_pair(p: tuple[Node, list[Node]]):
+        return HaskellFunction(*p)
+
+
+class AST:
+    """Helper Class to build/read/manipulate AST with tree-sitter"""
+
+    def __init__(self, source_code: str, lang: Language) -> None:
+        self.src = source_code
+        self.parser = Parser()
+        self.parser.set_language(lang)
+        self.tree: Tree = self.parser.parse(bytes(self.src, "utf8"))
+
+    @property
+    def root(self) -> Node:
+        return self.tree.root_node
+
+    def get_src_from_node(self, node: Node) -> str:
+        start = node.start_byte
+        end = node.end_byte
+        return self.src[start:end]
+
+    def get_fn_name(self, node: Node) -> Maybe[str]:
+        fn_name: str
+        match node.type:
+            case "signature":
+                signature_src = self.get_src_from_node(node)
+                fn_name = signature_src.split("::")[0]
+            case "function":
+                func_src = self.get_src_from_node(node)
+                fn_name = func_src.split(" ")[0]
+            case _:
+                return Nothing
+        return Some(fn_name.strip())
+
+    def get_fn_docstring(self, node: Node) -> Maybe[str]:
+        # todo: implement docstring finder
+        raise NotImplementedError
+
+    def func2src(self, func: HaskellFunction) -> str:
+        type_src = self.get_src_from_node(func.type_signature)
+        code_src = Chain(func.functions).map(self.get_src_from_node).value
+        code_src.sort()
+        return "\n".join([type_src, *code_src])
+
+    def get_functions(self) -> list[HaskellFunction]:
+        """extract functions from an AST
+
+        Args:
+            root (Node): root node of the AST
+
+        Returns:
+            list[HaskellFunction]: [(type signature, [function code])]
+        """
+        signatures = AST.get_all_nodes_of_type(self.root, "signature")
+        functions: dict[str, list[Node]] = (
+            Chain(AST.get_all_nodes_of_type(self.root, "function"))
+            .group_by_keys(lambda node: self.get_fn_name(node))
+            .value
+        )
+
+        def make_ty_fn_pair(type_signature: Node):
+            return (
+                self.get_fn_name(type_signature)
+                .map(lambda fn_name: (type_signature, functions[fn_name]))
+                .value_or(None)
+            )
+
+        pairs: list[HaskellFunction] = (
+            Chain(signatures)
+            .map(make_ty_fn_pair)
+            .filter(None)  # short for filter(lambda x: x is not None)
+            .map(HaskellFunction.from_pair)
+            .value
+        )
+        return pairs
+
+    @staticmethod
+    def get_all_nodes_of_type(
+        root: Node, node_type: str | None, max_level=50
+    ) -> list[Node]:
+        """walk on AST and collect all nodes of the given type
+
+        Args:
+            root (Node): root node of tree or subtree
+            node_type (str | None): type of node to collect, if None collect all Node
+            max_level (int, optional): maximum recursion level. Defaults to 50.
+
+        Returns:
+            list[Node]: collected nodes
+        """
+        nodes: list[Node] = []
+        if max_level == 0:
+            return nodes
+
+        for child in root.children:
+            if type is None or child.type == node_type:
+                nodes.append(child)
+            nodes += AST.get_all_nodes_of_type(
+                child, node_type, max_level=max_level - 1
+            )
+        return nodes
+
+    @staticmethod
+    def has_any_child_of_type(
+        root: Node, node_type: str | None, max_level: int = 50
+    ) -> bool:
+        """walk on AST and check if a node of `node_type` exists
+
+        Args:
+            root (Node): root node of tree or subtree
+            node_type (str | None): type of node to collect, if None collect all Node
+            max_level (int, optional): maximum recursion level. Defaults to 50.
+
+        Returns:
+            bool: `root` has and child of type `node_type`
+        """
+        if max_level == 0:
+            return False
+
+        has_child = False
+        for child in root.children:
+            if type is None or child.type == node_type:
+                return True
+            has_child |= AST.has_any_child_of_type(
+                child, node_type, max_level=max_level - 1
+            )
+        return has_child
