2091a69bc7c588670533eec04b72332bdb4537f8
Chris Gaw <christophercgaw@gmail.com>
2024-07-18 10:25:22 -0700

    Fixed test to be regardless of set order

--- tests/test_rewrite.py
+++ tests/test_rewrite.py
@@ -20,7 +20,7 @@ def test_rewrite_type():
             "(%) :: (Integral a) => a -> a -> Ratio a"
         ]
     }
-    result = {
+    valid_result1 = {
         "task_id": "data/repos/ghc-internal-9.1001.0/src/GHC/Internal/Real.hs--fromRational", 
         "signature": "fromRational :: A -> a", 
         "code": "fromRational (x:%y) = fromInteger x % fromInteger y", "poly_type": "Parametric", 
@@ -29,15 +29,24 @@ def test_rewrite_type():
             "(%) :: (Integral a) => a -> a -> Ratio a"
         ]
     }
+    valid_result2 = {
+        "task_id": "data/repos/ghc-internal-9.1001.0/src/GHC/Internal/Real.hs--fromRational", 
+        "signature": "fromRational :: B -> a", 
+        "code": "fromRational (x:%y) = fromInteger x % fromInteger y", "poly_type": "Parametric", 
+        "dependencies": [
+            "fromInteger :: A -> a", 
+            "(%) :: (Integral a) => a -> a -> Ratio a"
+        ]
+    }
 
     task = from_dict(data_class=BenchmarkTask, data=task_data)
 
     task = rewrite_type(task)
 
-    assert task.signature == result["signature"]
-    assert task.code == result["code"]
-    assert task.poly_type == result["poly_type"]
-    assert task.dependencies[0] == task.dependencies[0]
+    assert task.signature in [valid_result1["signature"], valid_result2["signature"]]
+    assert task.code in [valid_result1["code"], valid_result2["code"]]
+    assert task.poly_type == valid_result1["poly_type"]
+    assert task.dependencies[0] in [valid_result1["dependencies"][0], valid_result2["dependencies"][0]]
 
 
 def test_rewrite_func():
@@ -50,7 +59,17 @@ def test_rewrite_func():
             "Just :: a -> Maybe a"
         ]
     }
-    result = {
+    # Account for two results due to set order
+    valid_result1 = {
+        "task_id": "data/repos/ghc-internal-9.1001.0/src/GHC/Internal/Data/Maybe.hs--maybe",
+        "signature": "p :: b -> (a -> b) -> Maybe a -> b",
+        "code": "p _ f (q x) = f x\np n _ Nothing = n",
+        "poly_type": "Parametric",
+        "dependencies": [
+            "q :: a -> Maybe a"
+        ]
+    }
+    valid_result2 = {
         "task_id": "data/repos/ghc-internal-9.1001.0/src/GHC/Internal/Data/Maybe.hs--maybe",
         "signature": "q :: b -> (a -> b) -> Maybe a -> b",
         "code": "q _ f (p x) = f x\nq n _ Nothing = n",
@@ -64,10 +83,10 @@ def test_rewrite_func():
 
     task = rewrite_functions(task)
 
-    assert task.signature == result["signature"]
-    assert task.code == result["code"]
-    assert task.poly_type == result["poly_type"]
-    assert task.dependencies[0] == task.dependencies[0]
+    assert task.signature in [valid_result1["signature"], valid_result2["signature"]]
+    assert task.code in [valid_result1["code"], valid_result2["code"]]
+    assert task.poly_type == valid_result1["poly_type"]
+    assert task.dependencies[0] in [valid_result1["dependencies"][0], valid_result2["dependencies"][0]]
 
 
 if __name__ == "__main__":
