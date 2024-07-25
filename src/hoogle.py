a7c882f291faf0ad1ff7d60b70a96a9930d34fcd
Chris Gaw <christophercgaw@gmail.com>
2024-07-25 16:32:55 -0700

    feat: improved hoogle solver for more tasks

--- src/hoogle.py
+++ src/hoogle.py
@@ -10,6 +10,8 @@ from src.common import BenchmarkTask
 from src.filter2complete import extract_function_name
 from src.hs_parser import HASKELL_LANGUAGE
 from functools import lru_cache
+from tree_sitter import Node
+# Need to update requirements.txt
 
 def get_func_calls(task: BenchmarkTask) -> set[str]:
     """extract function calls and operators as string"""
@@ -19,13 +21,47 @@ def get_func_calls(task: BenchmarkTask) -> set[str]:
     ast = AST(task.code, HASKELL_LANGUAGE)
     root = ast.root
 
+    variables: list[str] = (
+        Chain(ast.get_all_nodes_of_type(root, "variable"))
+        .map(ast.get_src_from_node)
+        .filter(lambda x: x != fn_name)
+        .filter(lambda x: " " not in x)  # eliminate curried calls
+        .value
+    )
+    # Idea, Need some way of getting only variables that are functions to be called
+    # Also these functions need to be valid
+    patterns: list[Node] = (
+        Chain(ast.get_all_nodes_of_type(root, "patterns"))
+        .value
+    )
+
+    bindings: list[Node] = (
+        Chain(ast.get_all_nodes_of_type(root, "bind"))
+        .map(lambda node: node.child(0))  # variable is first child of binding
+        .value
+    )
+
+    generators: list[Node] = (
+        Chain(ast.get_all_nodes_of_type(root, "generator"))
+        .map(lambda node: node.child(0))  # variable is first child of binding
+        .value
+    )
+
+    ban_list: list[str] = []
+    for node in (patterns + bindings + generators):
+        nodes = ast.get_all_nodes_of_type(node, "variable")
+        ban_list += Chain(nodes).map(ast.get_src_from_node).value
+        if node.type == "variable":
+            ban_list += [ast.get_src_from_node(node)]
+
+    print(ban_list)
+
     calls: list[str] = (
         Chain(ast.get_all_nodes_of_type(root, "apply"))
         .map(lambda node: node.child(0))  # invoked function is the first child of apply
         .map(ast.get_src_from_node)
         .filter(lambda x: x != fn_name)
         .filter(lambda x: " " not in x)  # eliminate curried calls
-        .filter(lambda x: x != "f" and x != "g")
         .value
     )
 
@@ -34,11 +70,17 @@ def get_func_calls(task: BenchmarkTask) -> set[str]:
         .map(ast.get_src_from_node)
         .map(lambda x: f"({x})")  # infix operator . \equiv function (.)
         .filter(lambda x: x != fn_name)
-        .filter(lambda x: x != "f" and x != "g")
         .value
     )
 
-    return set(calls + operators)
+    final_list = (set(calls + operators + variables) - set(ban_list))
+
+    filtered_final_list = (Chain(final_list)
+    .filter(lambda d: not (len(d) == 1 and d.isalnum()))
+    .filter(lambda d: d != "xs")
+    .value)
+
+    return filtered_final_list
 
 def add_dependencies(task: BenchmarkTask)-> BenchmarkTask:
     depedencies = list(get_func_calls(task))
@@ -64,7 +106,7 @@ def add_dependencies(task: BenchmarkTask)-> BenchmarkTask:
 @lru_cache(maxsize=None)
 def get_type_signature(name: str) -> str | None:
     # api-endpoint
-    URL = "https://hoogle.haskell.org?mode=json&format=text&hoogle={0}&start=1&count=1".format(quote(name))
+    URL = "https://hoogle.haskell.org?mode=json&format=text&hoogle={0}+is%3Aexact&start=1&count=1".format(quote(name))
 
     # sending get request to get hoogle result
     r = requests.get(url = URL)
@@ -81,25 +123,25 @@ def get_type_signature(name: str) -> str | None:
     return type_signature
 
 def main(
-    input_file: str = "data/source/base-4.20.0.0_complete.jsonl",
-    output_file: str = "out.json",
+    input_file: str = "Benchmark-F.json",
+    output_file: str = "outv3.json",
 ):
     # For json files
-    # with open(input_file, "r") as fp:
-    #     tasks: list[BenchmarkTask] = (
-    #         Chain(json.load(fp))
-    #         .map(lambda d: from_dict(data_class=BenchmarkTask, data=d))
-    #         .value
-    #     )
-
-    # For jsonl files
     with open(input_file, "r") as fp:
         tasks: list[BenchmarkTask] = (
-            Chain(fp.readlines())
-            .map(json.loads)
+            Chain(json.load(fp))
             .map(lambda d: from_dict(data_class=BenchmarkTask, data=d))
             .value
         )
+
+    # For jsonl files
+    # with open(input_file, "r") as fp:
+    #     tasks: list[BenchmarkTask] = (
+    #         Chain(fp.readlines())
+    #         .map(json.loads)
+    #         .map(lambda d: from_dict(data_class=BenchmarkTask, data=d))
+    #         .value
+    #     )
 
     tasks_w_dep = (
         Chain(tasks)
