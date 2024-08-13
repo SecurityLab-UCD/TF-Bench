607efe262513106b5ef489acc8741d6dc7bb32db
Yifeng He <heeafen0602@gmail.com>
2024-08-13 08:15:25 +0800

    add: options to run ollama models in sizes

--- src/experiment_ollama.py
+++ src/experiment_ollama.py
@@ -22,46 +22,48 @@ from src.common import (
     get_prompt,
 )
 
+OLLAMA_LARGE = [
+    "gemma2:27b",
+    "llama2:70b",
+    "llama3:70b",
+    "mixtral:8x22b",
+    "deepseek-coder-v2:236b",
+    "codellama:34b",
+    "codellama:70b",
+    "phind-codellama:34b",
+    "granite-code:34b",
+    "codebooga:34b",
+    "nous-hermes2-mixtral:8x7b",
+    "codestral:22b",
+]
 
-OLLAMA_MODELS = [
+OLLAMA_SMALL = [
     "llama2:7b",
     "llama2:13b",
-    "llama2:70b",
     "llama3",
-    "llama3:70b",
     "phi3",
     "phi3:medium",
     "gemma:2b",
     "gemma:7b",
     "gemma2",
-    "gemma2:27b",
     "mistral",
-    "mixtral:8x22b",
     "mixtral:8x7b",
     "deepseek-coder-v2:16b",
-    "deepseek-coder-v2:236b",
     "codegemma:2b",
     "codegemma:7b",
     "codellama:7b",
     "codellama:13b",
-    "codellama:34b",
-    "codellama:70b",
     "starcoder2:3b",
     "starcoder2:7b",
     "starcoder2:15b",
-    "nous-hermes2:10.7b",
-    "nous-hermes2:34b",
-    "nous-hermes2-mixtral:8x7b",
-    "codestral:22b",
     "stable-code:3b",
     "codeqwen:7b",
-    "phind-codellama:34b",
     "granite-code:3b",
     "granite-code:8b",
     "granite-code:20b",
-    "granite-code:34b",
-    "codebooga:34b",
 ]
+
+OLLAMA_MODELS = OLLAMA_SMALL + OLLAMA_LARGE
 
 
 def get_model(
