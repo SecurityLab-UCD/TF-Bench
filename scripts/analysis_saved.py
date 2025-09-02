e35dc5f27713bbcfe40e4d0b708b3680dda8d727
Yifeng He <yfhe@ucdavis.edu>
2025-09-02 13:36:33 -0700

    fix: analysis_saved script

--- scripts/analysis_saved.py
+++ scripts/analysis_saved.py
@@ -7,16 +7,15 @@ from tfbench import (
     load_tfb_from_hf,
     load_gen_results_jsonl,
     evaluate,
-    LMAnswer,
 )
 
 
 def main(result_dir: str, log_file: str | None = None):
     """
-
     Arguments:
         result_dir (str): assumed in format `/some/path/.../<model>/<split>/`.
-            For example: results/gpt-5-nano-2025-08-07/base, where <model> is `gpt-5-nano-2025-08-07` and <split> is `base`.
+            For example: results/gpt-5-nano-2025-08-07/base,
+            where <model> is `gpt-5-nano-2025-08-07` and <split> is `base`.
             WARNING: we parse the  <model> and <split> in this way.
         log_file (str | None): path to the log file. If None, this script only prints to stdout.
     """
@@ -30,7 +29,7 @@ def main(result_dir: str, log_file: str | None = None):
     jsonl_files = [
         pjoin(result_dir, f) for f in os.listdir(result_dir) if f.endswith(".jsonl")
     ]
-    runs  = [load_gen_results_jsonl(f) for f in jsonl_files]
+    runs = [load_gen_results_jsonl(f) for f in jsonl_files]
     accs = [evaluate(tasks, run) for run in runs]
     mean, std = analysis_multi_runs(accs)
 
